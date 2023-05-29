2cd50189ff2972b08c623e49cff9abfc5e690485
yuricaprini <yuricaprini@gmail.com>
2023-05-29 12:06:13 +0200

    now produces executable jars with dependencies

---
+++ wordleclient/src/main/java/io/github/yuricaprini/wordleclient/ClientConfiguration.java
@@ -0,0 +1,9 @@
+package io.github.yuricaprini.winsomeclient;
+
+public class ClientConfiguration {
+
+  public String registryHost;
+  public Integer registryPort;
+  public String serverAddress;
+  public Integer serverPort;
+}
---
+++ wordleserver/src/main/java/io/github/yuricaprini/wordleserver/ServerMain.java
@@ -0,0 +1,13 @@
+package io.github.yuricaprini.wordleserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class ServerMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
